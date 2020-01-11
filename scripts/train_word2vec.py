0a6f12256759ee2421d58ace1c733d7e1c64361e
vt257 <vahe.tshitoyan@gmail.com>
2020-01-10 21:58:56 -0800

    Add a new validation set. Word2vec code cleanup.

--- scripts/train_word2vec.py
+++ scripts/train_word2vec.py
@@ -26,39 +26,40 @@ def main(args):
             compute_loss=True,
             sorted_vocab=True,
             iter=int(args.epochs))
-    my_model.save(args.model_file)
-    analogy_file = os.path.join("allnews_am/data", "yerevann_analogies.txt")
-    my_model.wv.accuracy(analogy_file)
+    my_model.save(
+        os.path.join(file_dir, '../allnews_am/models/', args.model_name))
+    analogy_file = os.path.join(
+        file_dir, '../allnews_am/data/yerevann_analogies.txt')
+    my_model.wv.evaluate_word_analogies(analogy_file)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-            "--corpus",
-            default=os.path.join(file_dir, "../allnews_am/data/corpus"),
-            help="The path to the corpus to train on.")
+            '--corpus',
+            default=os.path.join(file_dir, '../allnews_am/data/corpus'),
+            help='The path to the corpus to train on.')
     parser.add_argument(
-            "--model_file",
-            default=os.path.join("allnews_am/models", "word2vec.model"),
-            help="The path to the corpus to train on.")
-    parser.add_argument("--size", default=256, help="Size of the embedding.")
-    parser.add_argument("--window", default=8, help="Context window size.")
+            '--model_name', default='word2vec.model',
+            help='The name of the model file (saved in models folder).')
+    parser.add_argument('--size', default=100, help='Size of the embedding.')
+    parser.add_argument('--window', default=15, help='Context window size.')
     parser.add_argument(
-            "--min_count", default=5,
-            help="Minimum number of occurrences for word.")
+            '--min_count', default=5,
+            help='Minimum number of occurrences for word.')
     parser.add_argument(
-            "-sg", action="store_true",
-            help="If set, will train a skip-gram, otherwise a CBOW.")
-    parser.add_argument("--workers", default=4,
-                        help="Number of workers.")
-    parser.add_argument("--alpha", default=0.025,
-                        help="Learning rate.")
-    parser.add_argument("--negative", default=10,
-                        help="Number of negative samples.")
-    parser.add_argument("--epochs", default=30,
-                        help="Number of epochs.")
-    parser.add_argument("--subsample", default=0.001,
-                        help="Sub-sampling rate.")
+            '-sg', action='store_true',
+            help='If set, will train a skip-gram, otherwise a CBOW.')
+    parser.add_argument('--workers', default=4,
+                        help='Number of workers.')
+    parser.add_argument('--alpha', default=0.025,
+                        help='Learning rate.')
+    parser.add_argument('--negative', default=15,
+                        help='Number of negative samples.')
+    parser.add_argument('--epochs', default=30,
+                        help='Number of epochs.')
+    parser.add_argument('--subsample', default=0.0001,
+                        help='Sub-sampling rate.')
     word2vec_args = parser.parse_args()
     main(word2vec_args)
