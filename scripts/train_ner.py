83321355eba6b489fa4dd56907e7a0001a418469
vt257 <vahe.tshitoyan@gmail.com>
2020-01-20 00:15:07 -0800

    Add an option to use pre-trained embeddings for NER, and some more metrics.

--- scripts/train_ner.py
+++ scripts/train_ner.py
@@ -1,5 +1,8 @@
 """Train an NER model.
 
+Below are some scores for the "difficult test", where the result is only
+satisfactory if the full named entity is recognized.
+
 30 epochs with embedding/LSTM size 100/32 without any pre-trained embeddings:
  - f1: 56.34
            precision    recall  f1-score   support
@@ -11,13 +14,28 @@
 micro avg     0.5925    0.5370    0.5634      3138
 macro avg     0.5945    0.5370    0.5635      3138
 
+30 epochs with embedding/LSTM size 100/32 with pre-trained embeddings:
+fastText on 100k news articles.
+ - f1: 57.85
+           precision    recall  f1-score   support
+
+      LOC     0.6816    0.6160    0.6472      1849
+      PER     0.6523    0.5075    0.5709       865
+      ORG     0.3053    0.3420    0.3226       424
+
+micro avg     0.6112    0.5491    0.5785      3138
+macro avg     0.6227    0.5491    0.5823      3138
 """
+import argparse
 import os
 
 import tensorflow.compat.v2 as tf
 import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.metrics
 
 import allnews_am.processing
+import gensim.models
 from seqeval.callbacks import F1Metrics
 
 
@@ -31,11 +49,6 @@ COLUMN_TYPES = (
     allnews_am.processing.ConllReader.CHUNK,
     allnews_am.processing.ConllReader.NE,
 )
-
-BATCH_SIZE = 32  # Number of examples used in each learning step.
-EPOCHS = 30  # Number of passes through entire dataset.
-EMBEDDING = 100  # Dimension of word embedding vector.
-LSTM_SIZE = 32  # Dimension of the hidden/cell states of the LSTM network.
 
 
 def data_stats(conll_reader, data_suffix=''):
@@ -52,7 +65,7 @@ def data_stats(conll_reader, data_suffix=''):
     print(f"Example sentence ({data_suffix}): ", train_sentences[0])
 
 
-def main():
+def main(args):
     train_data_reader = allnews_am.processing.ConllReader(
         root=conll_data_root,
         fileids='train.conll03',
@@ -69,13 +82,14 @@ def main():
     train_words = train_data_reader.iob_words(column='ne')
     dev_sentences = dev_data_reader.iob_sents(column='ne')
 
-    # Plot the distribution of sentence lengths (in both train and dev).
-    sentence_lengths = [len(s) for s in train_sentences + dev_sentences]
-    plt.hist(sentence_lengths, bins=50)
-    plt.title(f'Tokens per sentence (max = {max(sentence_lengths)})')
-    plt.xlabel('Number of tokens (words)')
-    plt.ylabel('# samples')
-    plt.show()
+    if args.plot_seq_len:
+        # Plot the distribution of sentence lengths (in both train and dev).
+        sentence_lengths = [len(s) for s in train_sentences + dev_sentences]
+        plt.hist(sentence_lengths, bins=50)
+        plt.title(f'Tokens per sentence (max = {max(sentence_lengths)})')
+        plt.xlabel('Number of tokens (words)')
+        plt.ylabel('# samples')
+        plt.show()
 
     # The first 2 entries are reserved for PAD and UNK
     idx2word = ['PAD', 'UNK'] + list(set((t[0] for t in train_words)))
@@ -92,7 +106,7 @@ def main():
     print(f'The labels {test_tag} is identified by the index: '
           f'{tag2idx[test_tag]}')
 
-    def prepare_sentence_batches(sentences, batch_size=BATCH_SIZE):
+    def prepare_sentence_batches(sentences, batch_size=int(args.batch_size)):
         """Encodes sentences with strings and NE tags to numbers.
 
         Returns data as tensorflow tensor batches.
@@ -142,14 +156,28 @@ def main():
 
     # Model definition
     input_layer = tf.keras.layers.Input(shape=(None,))
-    x = tf.keras.layers.Embedding(
-        input_dim=len(word2idx),
-        output_dim=EMBEDDING,
-        mask_zero=True)(input_layer)
-    x = tf.keras.layers.Dropout(0.1)(x)
+    if args.pretrained_emb is not None:
+        emb_model = gensim.models.fasttext.FastText.load(args.pretrained_emb)
+        embedding_matrix = np.zeros((len(idx2word), emb_model.wv.vector_size))
+        for i, word in enumerate(idx2word):
+            embedding_matrix[i] = emb_model.wv[word]
+        x = tf.keras.layers.Embedding(
+            input_dim=len(word2idx),
+            output_dim=emb_model.wv.vector_size,
+            trainable=False,
+            weights=[embedding_matrix],
+            mask_zero=True)(input_layer)
+    else:
+        x = tf.keras.layers.Embedding(
+            input_dim=len(word2idx),
+            output_dim=int(args.embedding),
+            mask_zero=True)(input_layer)
+    x = tf.keras.layers.Dropout(float(args.emb_dropout))(x)
     x = tf.keras.layers.Bidirectional(
         tf.keras.layers.LSTM(
-            units=LSTM_SIZE, return_sequences=True, recurrent_dropout=0.1))(x)
+            units=int(args.lstm_size),
+            return_sequences=True,
+            recurrent_dropout=float(args.recurrent_dropout)))(x)
     x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(
         len(idx2tag), activation='softmax'))(x)
 
@@ -159,7 +187,7 @@ def main():
 
     history = model.fit(
         train_batches,
-        epochs=EPOCHS,
+        epochs=int(args.epochs),
         validation_data=dev_batches,
         callbacks=[
             F1Metrics({i: t for i, t in enumerate(idx2tag)},
@@ -167,6 +195,57 @@ def main():
         ],
     )
 
+    def print_classification_score(sentences):
+        y_true, y_pred = [], []
+        for i, sentence in enumerate(sentences):
+            y_true += [tag2idx[w[2]] for w in sentence]
+            x_true = [
+                [word2idx[w[0]] if w[0] in word2idx else 1 for w in sentence]
+            ]
+            y_pred += np.argmax(
+                model.predict_on_batch(np.asarray(x_true)), 2).tolist()[0]
+        print(sklearn.metrics.classification_report(
+            y_true, y_pred, labels=list(range(8)), target_names=idx2tag))
+
+    # These are the "easy scores", where it is not necessary to get the full
+    # Named entity to get a non-zero score.
+    print("Calculating training set metrics.")
+    print_classification_score(train_sentences)
+    print("Calculating dev set metrics.")
+    print_classification_score(dev_sentences)
+
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--pretrained_emb',
+        default=None,
+        # allnews_am/models/fastText_sg_100.model
+        help='The path to the pre-trained embeddings. If supplied, uses these '
+             'fixed embeddings instead of training them on the fly. This also '
+             'overrides the embedding dimension.')
+    parser.add_argument(
+        '--embedding', default=100,
+        help='The dimension of the embeddings.')
+    parser.add_argument(
+        '--epochs', default=30,
+        help='The number of training epochs (passes through entire dataset).')
+    parser.add_argument(
+        '--lstm_size', default=32,
+        help='Dimension of the hidden/cell states of the LSTM network.')
+    parser.add_argument(
+        '--emb_dropout', default=0.1,
+        help='The dropout applied right after the embedding layer.')
+    parser.add_argument(
+        '--recurrent_dropout', default=0.1,
+        help='The recurrent dropout of the LSTM layer.')
+    parser.add_argument(
+        '--batch_size', default=32,
+        help='Number of examples used in each learning step.')
+    parser.add_argument(
+        '-plot_seq_len', action='store_true',
+        help='If set, plots distribution of sequence lengths before the '
+             'training starts. Would require you to manually close the '
+             'matplotlib window to continue the execution of the script.')
+    main(parser.parse_args())
